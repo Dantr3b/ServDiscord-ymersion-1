3e33c0177ebb2fe8c2c8a2ce1154a7d1274aa14e
Dantr3b <94704718+Dantr3b@users.noreply.github.com>
2023-09-25 14:55:01 +0200

    sa essaye des truc

--- botdiscord.py
+++ botdiscord.py
@@ -15,4 +15,4 @@ class MyClient(discord.Client):
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
-client.run('token')
+client.run('1154767346121060403')
